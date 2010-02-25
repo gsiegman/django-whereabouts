42ab85961886b6dea11f69589c220e81dac7e237
Glenn Siegman <glenn.siegman@mwcradio.com>
2010-02-25 17:56:10 -0600

    added missing comma in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='django-whereabouts',
         version='0.1',
-        description='A more flexible django "elsewhere" app.'
+        description='A more flexible django "elsewhere" app.',
         author='Glenn Siegman',
         author_email='gsiegman@gsiegman.com',
         url='http://github.com/gsiegman/django-whereabouts',
