87b57933dc61f1b9158f3051ea60a4ed66968259
Glenn Siegman <glenn.siegman@mwcradio.com>
2010-02-26 11:13:29 -0600

    added admin to installed apps

--- example_project/settings.py
+++ example_project/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
     'south',
     'whereabouts',
 )
