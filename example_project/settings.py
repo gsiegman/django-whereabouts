a113e1b717f8aa04c856dd25f099e049acc9a5cb
Glenn Siegman <gsiegman@Glenn-Siegmans-MacBook-Pro.local>
2010-03-11 18:05:21 -0600

    corrected settings.py in example project

--- example_project/settings.py
+++ example_project/settings.py
@@ -12,12 +12,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'db_whereabouts'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'pg_bachiguala'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'access123!'         # Not used with sqlite3.
-DATABASE_HOST = 'ec2-174-129-74-15.compute-1.amazonaws.com'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = '5432'             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'whereabouts'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
