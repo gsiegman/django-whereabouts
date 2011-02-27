08559749dc8eb4b8a2d1113ee29939df5f877042
Glenn Siegman <gsiegman@gsiegman.com>
2011-02-27 09:54:22 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-whereabouts',
-        version='0.3',
+        version='0.4',
         description='A more flexible django "elsewhere" app.',
         author='Glenn Siegman',
         author_email='gsiegman@gsiegman.com',
